a212229c40c408dd63fdf384418e9c5d318687ab
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-05-19 18:55:16 -0500

    calling google api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -16,5 +16,9 @@ export default {
   // Saves a book to the database
   saveBook: function(bookData) {
     return axios.post("/api/books", bookData);
+  },
+
+  googleSearch: function(input){
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q=" +input)
   }
 };
